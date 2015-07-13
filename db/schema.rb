1b842749c041ccd626bffe08a3e22611dfa9fd28
danlyg <gih.daniel@gmail.com>
2015-07-12 17:11:23 -0700

    added birthdate and age to schema for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712015709) do
+ActiveRecord::Schema.define(version: 20150713001020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20150712015709) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "email"
+    t.date     "birthdate"
+    t.integer  "age"
   end
 
 end
