eed2826dd871cfd8fe3872554b7cc1a1fcb308bb
danlyg <gih.daniel@gmail.com>
2015-07-14 16:40:02 -0700

    fixing changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,24 +1,26 @@
 var ready = function(){
 
+  // $('.week').click(function() {
+  //   var weekId = parseInt(this.id.slice(4, this.id.length))
+  //   console.log(weekId)
+  // })
 
-//   function weekClick() {
-//     $('.week').click(function(){
-//       $(this).off('click')
-//       $(this).append("<div class='tooltip'><button class='close'>X</button></div>");
-//       $('.close').click(function(){
-//         $week = $(this).parents('.week')
-//         $week.on('click', weekClick)
-//         $tooltip = $(this).parent('.tooltip')
-//         $tooltip.remove()
-//       });
-//     });
-//   }
+
+
+  function weekClick() {
+    $('.week').click(function(){
+      $(this).off('click')
+      $(this).append("<div class='tooltip'><button class='close'>X</button></div>");
+      $('.close').click(function(){
+        $week = $(this).parents('.week')
+        $week.on('click', weekClick)
+        $tooltip = $(this).parent('.tooltip')
+        $tooltip.remove()
+      });
+    });
+  }
   
-//   weekClick();
-
-
-// }
-
+  weekClick();
 
 
 }
