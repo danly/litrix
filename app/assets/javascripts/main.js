52f50ec490897a8f78d44d547a2ba9fb9f092978
danlyg <gih.daniel@gmail.com>
2015-07-14 15:19:25 -0700

    worked on jquery also added material-design-lite

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,31 @@
+var ready = function(){
+
+
+//   function weekClick() {
+//     $('.week').click(function(){
+//       $(this).off('click')
+//       $(this).append("<div class='tooltip'><button class='close'>X</button></div>");
+//       $('.close').click(function(){
+//         $week = $(this).parents('.week')
+//         $week.on('click', weekClick)
+//         $tooltip = $(this).parent('.tooltip')
+//         $tooltip.remove()
+//       });
+//     });
+//   }
+  
+//   weekClick();
+
+
+// }
+
+
+
+}
+
+
+
+$(function() {
+  ready();
+});
+
