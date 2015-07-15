39eada3ee81c525fa61da613653e90d751429e9c
danlyg <gih.daniel@gmail.com>
2015-07-14 20:58:16 -0700

    able to milestone data with id and append it to the tooltip

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,7 @@
 var ready = function(){
   $('.week').on('click', weekClick)
+
+
 }
 
 function isEmpty($element){
@@ -9,14 +11,29 @@ function isEmpty($element){
 function weekClick(){
   if( isEmpty( $(this) ) ) {
     $(this).append("<div class='tooltip'><button class='close'>X</button></div>");
+
+    // need to do a get to add data
+    weekId = this.id.slice(4,this.id.length)
+    console.log(weekId)
+
+    var promise = $.getJSON("/milestones/"+weekId)
+    //If success
+    promise.done(function( data ){
+      $('.tooltip').append(data.title)
+    })
+    //If no data failure
+    promise.fail(function(){
+      $('.tooltip').append("No Personal Milestones")
+    })
+
     $('.close').click(function(event){
       event.stopPropagation();
       $week = $(this).parent().parent()
       $week.empty();
+
     })
   } else {
     console.log('not empty')
-
   }
 }
 
